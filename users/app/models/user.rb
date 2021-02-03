3cfcfc351ec35e2f36eb15a339984752e853fc5d
Lucas Santos da Costa <lsantosc@live.com>
2021-02-03 09:15:31 -0300

    add bunny, and prevent to publish id

--- users/app/models/user.rb
+++ users/app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   after_save :publish!
 
   def publish!
-    BunnyClient.push(attributes.to_json, self.class.name)
+    BunnyClient.push(to_json(except: [:id, :created_at, :updated_at]), self.class.name)
   end
 end
--- users/lib/bunny_client.rb
+++ users/lib/bunny_client.rb
@@ -6,13 +6,13 @@ class BunnyClient
       @connection = Bunny.new(ENV['CLOUDAQMP_URL'])
       @connection.start
       @channel = @connection.create_channel
-      @fanout = @channel.fanout('aceleracao_dio_users.out')
+      @fan_out = @channel.fanout('aceleracao_dio_users.out')
       @connected = true
     end
 
     def push(payload, type)
       connect! unless @connected
-      @fanout.publish(payload, { app_id: 'aceleracao_dio_users', type: type })
+      @fan_out.publish(payload, { app_id: 'aceleracao_dio_users', type: type })
 
       true
     end
